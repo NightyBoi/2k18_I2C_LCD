f24bf6773f4bab021210c6dd493f13f21535e540
DJ <33149171+PlaguedByWeirdness@users.noreply.github.com>
2017-11-24 00:22:52 +0100

    Comments added

--- MasterExample/3DigitScore.ino
+++ MasterExample/3DigitScore.ino
@@ -1,14 +1,13 @@
 #include <Wire.h>
 
 void setup() {
-  Wire.begin(); 				// Join i2c bus (address optional for master)
-  Serial.begin(9600);
+  Wire.begin(); 				      // Join i2c bus (address optional for master)
 }
 
-int score = 650;
+int score = 650;              // Change to any number you want to display
 
 void loop() {
-  byte x,y,z;
+  byte x,y,z;                 // Three variables in which we store every digit seperately (since max byte value is 127)
   x=score/100;
   y=(score/10)%10;
   z=score%10;
@@ -16,9 +15,9 @@ void loop() {
   Wire.beginTransmission(1); 	// Transmit to device #1
   Wire.write(x);            	// Sends first score digit
   delay(100);
-  Wire.write(y);				// Sends second score digit
+  Wire.write(y);			      	// Sends second score digit
   delay(100); 
-  Wire.write(z); 				// sends third score digit
+  Wire.write(z); 				      // Sends third score digit
   Wire.endTransmission();    	// Stop transmitting
   
  
