f2413ddb34202c106dd164ffa532d33c722a7d29
DJ <33149171+PlaguedByWeirdness@users.noreply.github.com>
2017-11-24 01:02:29 +0100

    Updating comments

--- main.ino
+++ main.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-#define LCD_ADDRESS 0x3E
+#define LCD_ADDRESS 0x3E			//Address of the LCD you are using, change it to yours
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #define printByte(args)  write(args);
@@ -10,25 +10,25 @@
 #endif
 
 
-byte x,y,z;
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
+byte x,y,z;					// Variables which we read from master
+// Function that executes whenever data is received from master
+// This function is registered as an event, see setup()
 void receiveEvent(int msg_length) {
-  while (1 < Wire.available()) { // loop through all but the last
-    x = Wire.read();
-    y = Wire.read();
-    z = Wire.read();
+  while (1 < Wire.available()) { // Loop while master is sending something
+    x = Wire.read();				// Read first digit
+    y = Wire.read();				// Read second digit
+    z = Wire.read();				// Read third digit
     delay(100);
   }
 }
-						//Header for this shit will be made soon
 
+//Making custom characters, 8 of them is maximum, so we made it so every number can be made with combination of these 8 parts
 byte bar1[8] = 
 {
         B11110,
         B11110,
         B11110,
-        B11110,
+        B11110,			// 1's will be shown on the lcd, 0's aren't on
         B11110,
         B11110,
         B11110,
@@ -39,7 +39,7 @@ byte bar2[8] =
         B01111,
         B01111,
         B01111,
-        B01111,
+        B01111,			// 1's will be shown on the lcd, 0's aren't on
         B01111,
         B01111,
         B01111,
@@ -50,7 +50,7 @@ byte bar3[8] =
         B11111,
         B11111,
         B00000,
-        B00000,
+        B00000,			// 1's will be shown on the lcd, 0's aren't on
         B00000,
         B00000,
         B11111,
@@ -61,7 +61,7 @@ byte bar4[8] =
         B11110,
         B11110,
         B00000,
-        B00000,
+        B00000,			// 1's will be shown on the lcd, 0's aren't on
         B00000,
         B00000,
         B11110,
@@ -72,7 +72,7 @@ byte bar5[8] =
         B01111,
         B01111,
         B00000,
-        B00000,
+        B00000,			// 1's will be shown on the lcd, 0's aren't on
         B00000,
         B00000,
         B01111,
@@ -83,7 +83,7 @@ byte bar6[8] =
         B00000,
         B00000,
         B00000,
-        B00000,
+        B00000,			// 1's will be shown on the lcd, 0's aren't on
         B00000,
         B00000,
         B11111,
@@ -94,7 +94,7 @@ byte bar7[8] =
         B00000,
         B00000,
         B00000,
-        B00000,
+        B00000,			// 1's will be shown on the lcd, 0's aren't on
         B00000,
         B00000,
         B01111,
@@ -105,7 +105,7 @@ byte bar8[8] =
         B11111,
         B11111,
         B00000,
-        B00000,
+        B00000,			// 1's will be shown on the lcd, 0's aren't on
         B00000,
         B00000,
         B00000,
@@ -114,7 +114,7 @@ byte bar8[8] =
 
 
 void custom0(int col)
-{ // uses segments to build numbers/letters
+{ // Uses segments to build numbers/letters, in this case numbers from 0 to 9
   lcd.setCursor(col, 0); 
   lcd.write(2);  
   lcd.write(8); 
@@ -232,7 +232,7 @@ void custom9(int col)
   lcd.write(6);  
   lcd.write(1);
 }
-
+	// Function which determines which number we are going to print and in which col
 void printNumber(int value, int col) {
   if (value == 0) {
     custom0(col);
@@ -261,12 +261,13 @@ LiquidCrystal_I2C lcd(LCD_ADDRESS,16,2);
 
 void setup()
 {
-  lcd.init();                      
-  lcd.backlight();
+  lcd.init();			// Defining lcd for usage
+  lcd.backlight();		// Activating lcd's backlight
 
-  Wire.begin(1); 
-  Wire.onReceive(receiveEvent);
+  Wire.begin(1); 		// Start the i2c, taking the address of 1 (slaves must have an address, masters nope)
+  Wire.onReceive(receiveEvent); // When slave recieves signal from the master, recieveEvent happens
        
+				// Making the segments possible to use with createChar
   lcd.createChar(1,bar1);
   lcd.createChar(2,bar2);
   lcd.createChar(3,bar3);
@@ -276,6 +277,7 @@ void setup()
   lcd.createChar(7,bar7);
   lcd.createChar(8,bar8);
 
+				// Using segments to make letter E 
   lcd.setCursor(0, 0); 
   lcd.write(2);  
   lcd.write(3); 
@@ -284,7 +286,7 @@ void setup()
   lcd.write(2);  
   lcd.write(6);  
   lcd.write(6);
-
+				// Using segments to make letter P
   lcd.setCursor(3, 0); 
   lcd.write(2);  
   lcd.write(3); 
@@ -293,9 +295,9 @@ void setup()
   lcd.write(2);  
   
   while(1){
-   printNumber(x, 7);
-   printNumber(y, 10);
-   printNumber(z, 13);
+   printNumber(x, 7);		//Printing digit x, starting on col 7
+   printNumber(y, 10);		//Printing digit y, starting on col 10
+   printNumber(z, 13);		//Printing digit z, starting on col 13
     delay(1000);
   }
 }
